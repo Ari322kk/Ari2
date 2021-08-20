ec6e68ba97fcf1b6d837b283ddcdfb28e6805741
bayel <arietrazzak@gmail.com>
2021-08-20 12:17:06 +0600

    H/w done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,45 +1,43 @@
 package com.company;
 
 import java.security.SecureRandom;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String value = "Красный";
-        switch (value){
-            case "Красный":  //case это "случай"
-                System.out.println("Вы агрессивный и вспыльчивый");
-                break;
-            case "Оранжевый":
-                System.out.println("Вы очень веселый");
-                break;
-            case "Белый":
-                System.out.println("Вы добрый и мягкий");
-                break;
 
+        System.out.println(sum(generateRandomAge(), 29));
+        System.out.println(sum(generateRandomAge(), 19));
+        System.out.println(sum(generateRandomAge(), 5));
+        System.out.println(sum(generateRandomAge(), 28));
+        System.out.println(sum(generateRandomAge(), 29));
 
+    }
 
+    public static String sum(int age, int temperature) {
+
+        if (age >= 20 && age <= 45 && temperature > -20 && temperature < 30) {
+            System.out.println("Можно идти гулять");
+        } else if (age < 20 && temperature > 0 && temperature < 28) {
+            System.out.println("Можно идти гулять1");
+        } else if (age > 45 && temperature > -10 && temperature < 25) {
+            System.out.println("Можно идти гулять2");
+        } else {
+            System.out.println("Оставайся дома");
         }
-        if(value=="Оранжевый" || value.length()>3) {
-            printText("Оранжевый", "this is color", 662);
-            System.out.println(sum(33,22));
+        return "Age " + age + " Temperature " + temperature;
+    }
 
-        }
-    }
-    public static  void print(){
-        System.out.println("Проверка прошла");
-        System.out.println("Проверка прошла");
-        System.out.println("Проверка прошла");
-    }
-    public static  void  printText(String text, String SecondText,int a){
-        System.out.println(text+" "+SecondText+" "+a);
-        System.out.println("green");
-    }
-    public static int sum(int a ,int b){
-        return a*b+(a*b+a*b);
-
+    public static int generateRandomAge() {
+        Random random = new Random();
+        int a = random.nextInt(81);
+        System.out.println(a);
+        return a;
     }
 }
+
 
 
 
